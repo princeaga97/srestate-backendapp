d5a67fd2aef18a64877f8e39ea0cba7abe23319f
princeaga97 <prince.agarwal@delhivery.com>
2022-06-28 22:21:08 +0530

    adding logs for future

--- chat/serializers.py
+++ chat/serializers.py
@@ -8,21 +8,30 @@ from chat.models import Contacts, Messages
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    def create(self,validate_data):
-        return Messages.objects.create(**validate_data)
+
+    
 
     class Meta:
         model = Messages
         exclude = ["sent","sender_name","time","timestamp"]
 
 class MessageViewSerializer(serializers.ModelSerializer):
+    timestamp = serializers.SerializerMethodField()
+
     class Meta:
         model = Messages
         fields = "__all__"
+    
+    def get_timestamp(self, obj):
+        return obj.timestamp.total_seconds()
 
 
 class ContactViewSerializer(serializers.ModelSerializer):
+    timestamp = serializers.SerializerMethodField()
     last_message = MessageSerializer()
+    
     class Meta:
         model = Contacts
         fields = "__all__"
+    def get_timestamp(self, obj):
+        return obj.timestamp.total_seconds()
