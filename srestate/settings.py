27cd2f453eb8f1bf6fa3f7217b633a0e916f6bf2
princeaga97 <prince.agarwal@delhivery.com>
2022-07-05 22:54:48 +0530

    make it better

--- srestate/settings.py
+++ srestate/settings.py
@@ -113,13 +113,9 @@ DATABASES = {
         },
 
         'messagedb':{
-            "ENGINE": 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DATABASE_NAME'),
-            'USER': os.environ.get('DATABASE_USER'),
-            'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-            'HOST': os.environ.get('DATABASE_HOST'),
-            'PORT': '5432'
-            },
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        },
         'db2': {
             'ENGINE': 'djongo',
             'NAME': 'your-db-name',
