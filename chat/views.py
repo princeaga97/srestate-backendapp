287136e4592760b40c34035bf5386106682e2cc2
princeaga97 <prince.agarwal@delhivery.com>
2022-07-08 13:11:15 +0530

    make it better

--- chat/views.py
+++ chat/views.py
@@ -27,14 +27,13 @@ import websockets
 @async_to_sync
 async def send_ws(sender,From,message):
     async with websockets.connect(f"wss://srestatechat.herokuapp.com/ws/chat/{sender}_{From}/") as websocket:
-        while 1:
-            try:
-                #a = readValues() #read values from a function
-                #insertdata(a) #function to write values to mysql
-                await websocket.send('{"message":{"'+ message + '"}')
-                print("send")
-            except Exception as e:
-                print(e)
+        try:
+            #a = readValues() #read values from a function
+            #insertdata(a) #function to write values to mysql
+            await websocket.send('{"message":{"'+ message + '"}')
+            print("send")
+        except Exception as e:
+            print(e)
 
 def create_msg_in_db(data,sender,recieved = False):
     serilizer = MessageSerializer(data=data)
